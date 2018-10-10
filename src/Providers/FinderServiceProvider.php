4bc8a1cc72450bcddba9809f5cb5f8a261968d43
cytRasch <sebastian.rasch@a-k-f.de>
2018-10-10 10:57:44 +0200

    =Change plugin.json

--- src/Providers/FinderServiceProvider.php
+++ src/Providers/FinderServiceProvider.php
@@ -4,9 +4,7 @@
 namespace Finder\Providers;
 
 
-use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\Templates\Twig;
 
 
 /**
@@ -19,29 +17,12 @@ class FinderServiceProvider extends ServiceProvider
 {
 
     /**
-     * Template priority
-     */
-    CONST ORDER_NOW_PRIORITY = 0;
-
-
-    /**
      *
      */
     public function register()
     {
 
         $this->getApplication()->register(FinderRouteServiceProvider::class);
-
-    }
-
-
-    /**
-     * @param \Plenty\Plugin\Events\Dispatcher $dispatcher
-     * @param \Plenty\Plugin\Templates\Twig    $twig
-     */
-    public function boot( Dispatcher $dispatcher, Twig $twig )
-    {
-
     }
 
 
